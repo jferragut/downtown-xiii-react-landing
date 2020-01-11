1439885026e52e74b8ddce803688fe26bfc1fc4d
aleaguilar <alejandroalbertoaguilar@gmail.com>
2020-01-11 17:03:19 +0000

    navbar with container-fluid

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 //create your first component
 export function Navbar() {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="container-fluid navbar navbar-expand-lg navbar-dark bg-dark">
 			<a className="navbar-brand" href="#">
 				Start Bootstrap
 			</a>
